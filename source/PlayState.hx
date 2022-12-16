8581565f30fdd260775b010450a7eec5e9483ffb
Foxel <kegee55@gmail.com>
2022-12-16 01:49:48 -0500

    too much. shit (i keep forgetting to commit LMAO)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,19 +3,28 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.addons.ui.FlxInputText;
+import flixel.group.FlxSpriteGroup;
 import flixel.input.keyboard.FlxKey;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
 	var guessedNames:Array<String> = [];
 	var names:Array<String>;
 
+	var inputGroup:FlxSpriteGroup = new FlxSpriteGroup();
+
 	var input:FlxInputText = new FlxInputText(0, 0, 150, "", 8);
 
 	var completionAmount:FlxText = new FlxText(0, 0, 0, "0/?", 50);
+	var at:FlxText = new FlxText(0, 0, 0, "@", 25);
+
+	var baldiTimer = new FlxTimer();
+	var baldiTimerStarted:Bool = false;
+	var baldiCount:Int;
 
 	override public function create()
 	{
@@ -29,7 +38,15 @@ class PlayState extends FlxState
 
 		input.screenCenter();
 		input.y += 150;
-		add(input);
+		input.updateHitbox();
+		inputGroup.add(input);
+
+		at.x = input.x - input.scale.x - 25;
+		at.screenCenter(Y);
+		at.y += 150;
+		inputGroup.add(at);
+
+		add(inputGroup);
 
 		input.focusGained = function():Void
 		{
@@ -70,8 +87,6 @@ class PlayState extends FlxState
 
 	function guessName(input:String)
 	{
-		trace(input);
-
 		var foundName = false;
 		var alreadyGuessed = false;
 
@@ -101,6 +116,7 @@ class PlayState extends FlxState
 		else if (alreadyGuessed)
 		{
 			notif("alreadyGuessed");
+			trace(input);
 		}
 		else
 		{
@@ -115,19 +131,45 @@ class PlayState extends FlxState
 	{
 		var notifText:FlxText = new FlxText(0, 0, 0, "You shouldn't be able to read this.", 15);
 
+		if (type == "found")
+		{
+			baldiCount += 1;
+
+			if (baldiTimerStarted == false)
+			{
+				baldiTimerStarted = true;
+				baldiTimer = new FlxTimer().start(6.5, function(timer:FlxTimer)
+				{
+					baldiCount = 0;
+					baldiTimerStarted = false;
+				});
+			}
+
+			if (baldiCount == 3)
+			{
+				baldiCount = 0;
+				FlxG.sound.play("assets/sounds/fantastic.ogg");
+				baldiTimer.cancel();
+				baldiTimerStarted = false;
+			}
+		}
+
 		switch type
 		{
 			case "found":
 				FlxG.sound.play("assets/sounds/correct.ogg");
-				notifText.text = "Found!";
+				notifText.text = "Correct!";
 				notifText.color = FlxColor.GREEN;
 			case "alreadyGuessed":
+				FlxG.sound.play("assets/sounds/alreadyGuess.ogg");
 				notifText.text = "Already Guessed!";
 				notifText.color = FlxColor.YELLOW;
 			case "notInNames":
+				FlxG.sound.play("assets/sounds/incorrect.ogg");
 				notifText.text = "Not in Blocklist!";
 				notifText.color = FlxColor.RED;
 			case "empty":
+				FlxG.sound.play("assets/sounds/empty.ogg");
 				notifText.text = "You need to type something first, silly!";
 				notifText.color = FlxColor.ORANGE;
 			default:

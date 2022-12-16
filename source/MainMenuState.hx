d579d4724327c4710b671b4bcc6b46f8777e681d
Foxel <kegee55@gmail.com>
2022-12-16 11:18:58 -0500

    guthib

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -4,15 +4,18 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.addons.ui.FlxUIButton;
+import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 
 class MainMenuState extends FlxState
 {
 	var elapsedTime:Float;
 
 	var icon:FlxSprite;
+	var github:FlxSprite;
 
 	override public function create()
 	{
@@ -49,6 +52,14 @@ class MainMenuState extends FlxState
 		startButton.y = 500;
 		add(startButton);
 
+		github = new FlxSprite().loadGraphic("assets/images/github.png");
+		github.setGraphicSize(Std.int(icon.width * 0.05));
+		github.updateHitbox();
+		github.antialiasing = true;
+		github.y -= 100;
+		github.x = 5;
+		add(github);
+
 		FlxTween.tween(text, {y: 48}, 4, {
 			ease: FlxEase.elasticOut,
 			onComplete: function(twn:FlxTween)
@@ -57,6 +68,7 @@ class MainMenuState extends FlxState
 			}
 		});
 		FlxTween.tween(icon, {x: -180}, 4, {ease: FlxEase.elasticOut});
+		FlxTween.tween(github, {y: 5}, 4, {ease: FlxEase.elasticOut});
 		FlxTween.tween(startButton, {y: 402}, 4, {
 			ease: FlxEase.elasticOut,
 			onComplete: function(twn:FlxTween)
@@ -69,6 +81,22 @@ class MainMenuState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.mouse.overlaps(github))
+		{
+			github.color = FlxColor.WHITE;
+			github.scale.x = (FlxMath.lerp(github.scale.x, 0.12, 0.1));
+			github.scale.y = (FlxMath.lerp(github.scale.y, 0.12, 0.1));
+
+			if (FlxG.mouse.justPressed)
+				FlxG.openURL("https://github.com/FoxelTheFennic/DNBBlocklistGuessing");
+		}
+		else
+		{
+			github.color = FlxColor.GRAY;
+			github.scale.x = (FlxMath.lerp(github.scale.x, 0.098, 0.1));
+			github.scale.y = (FlxMath.lerp(github.scale.y, 0.098, 0.1));
+		}
 	}
 
 	function onClick()
